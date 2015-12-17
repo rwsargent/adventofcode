bfbdc91b6a2be9197700e7bbf82d1c6d90d32bb9
rwsargent <rw.sarge@gmail.com>
2015-12-17 16:33:59 -0700

    started day8

---
+++ day8.py
@@ -0,0 +1,20 @@
+import re
+def read_input(filename):
+    string_size = 0, memory_size = 0
+    hexi = re.compile("\\[a-f]{2}")
+    espcae
+    with open(filename) as file:
+        for line in file:
+            string_size += len(line)
+            line = hexi.replace(line, "*")
+            qa
+    return None
+        
+def execute_part_one(input):
+    return None
+
+def execute_part_two(input):
+    return None
+
+def get_expected_results_map():
+    return { 'test.txt' : (None, None)}
