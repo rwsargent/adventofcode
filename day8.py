6ec81cd8f0d04e0f7f07d510cc713c4f84eb4840
Ryan S <rwsarge@gmail.com>
2015-12-19 14:47:01 -0700

    day 8 finished

--- day8.py
+++ day8.py
@@ -1,24 +1,32 @@
 import re
 def read_input(filename):
-    string_size = 0
-    memory_size = 0
-    hexi = re.compile(r"(\\x[a-f0-9]{2})")
-    escape = re.compile(r"((?<=[^\\]\\).)")
+    lines = []
     with open(filename) as file:
         for line in file:
-            line = line.strip()
-            string_size += len(line)
-            line = hexi.sub("*", line)
-            line = escape.sub("", line)[1:-1]
-            memory_size += len(line)
-    return string_size, memory_size
-        
+            lines.append(line.strip())
+    return lines
+
 def execute_part_one(input):
-    string, mem = input
-    return string - mem
+    string_size = 0
+    memory_size = 0
+    backslashes = re.compile(r'\\\\')
+    quotes = re.compile(r'(\\")')
+    hexi = re.compile(r"(?<!\\)(\\x[a-f0-9]{2})")
+    for line in input:
+        string_size += len(line)
+        line = backslashes.sub('*', line)[1:-1]
+        line = quotes.sub('*', line)
+        line = hexi.sub("*", line)
+        memory_size += len(line)
+    return string_size - memory_size
 
 def execute_part_two(input):
-    return None
-
+    acc = 0
+    for line in input:
+        acc += 2+line.count('\\')+line.count('"')
+    return acc
+    
+                 
+               
 def get_expected_results_map():
-    return { 'test.txt' : (12, None)}
+    return { 'test.txt' : (12, 19)}
