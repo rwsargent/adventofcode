82ec9da218f72f628491937750093dafd82433ce
Ryan S <rwsarge@gmail.com>
2015-12-26 13:27:43 -0800

    day17

--- day17.py
+++ day17.py
@@ -1,3 +1,4 @@
+import itertools
 def read_input(filename):
     size_array = []
     with open(filename) as file:
@@ -6,10 +7,25 @@ def read_input(filename):
     return size_array
         
 def execute_part_one(input):
-    return recurs_fill(input, 0, 150)
+    ps =  [x for x in powerset(input)]
+    counter =0
+    for pset in ps:
+        acc = sum(pset)
+        if (acc == 150):
+            counter += 1
+    return counter
 
 def execute_part_two(input):
-    return None
+    ps =  [x for x in powerset(input)]
+    size_map = [0 for x in range(len(input)+1)]
+    for pset in ps:
+        acc = sum(pset)
+        if (acc == 150):
+            size_map[len(pset)] += 1
+    #return the first non-zero number
+    for x in size_map:
+        if x != 0:
+            return x
 
 def get_expected_results_map():
     return { 'test.txt' : (None, None)}
@@ -17,10 +33,18 @@ def get_expected_results_map():
 def recurs_fill(sizes, i, total):
     if (i >= len(sizes) or total < 0):
         return 0
-    if (total == 0):
+    if (total-sizes[i] == 0):
         return 1
     return recurs_fill(sizes, i+1, total-sizes[i]) + recurs_fill(sizes, i+1, total)
     
-    
-            
-    
+def powerset(seq):
+    """
+    Returns all the subsets of this set. This is a generator.
+    """
+    if len(seq) <= 1:
+        yield seq
+        yield []
+    else:
+        for item in powerset(seq[1:]):
+            yield [seq[0]]+item
+            yield item
