fdca6b07a604fc60e53fc0a70a0c9dbbd8a9961a
Ryan S <rw.sarge@gmail.com>
2016-12-20 09:49:32 -0700

    dat 8 ssetup

---
+++ 2016/day8.py
@@ -0,0 +1,14 @@
+def read_input(filename):
+    with open(filename) as file:
+        for line in file:
+            pass
+    return None
+        
+def execute_part_one(input):
+    return None
+
+def execute_part_two(input):
+    return None
+
+def get_expected_results_map():
+    return { 'test.txt' : (None, None)}
