9ada930032f61a4d0a138b7c7de486b853db01c4
Ryan S <rw.sarge@gmail.com>
2016-12-20 09:49:20 -0700

    day7

--- 2016/day7.py
+++ 2016/day7.py
@@ -1,18 +1,31 @@
 import re
-abba_regex = re.compile(r'((\w)(?!\2)(\w)\3\2)(?![^{]*})')
+import pdb
+regex_string = r'((\w)(?!\2)(\w)\3\2)(?![^\[]*\])'
+inner_regex = re.compile(r'((\w)(?!\2)(\w)\3\2)(?=[^\[]*\])')
+abba_regex = re.compile(regex_string)
 
 def read_input(filename):
   with open(filename) as file:
     return [line for line in file]
         
 def execute_part_one(ipv7s):
+  count = 0
   for address in ipv7s:
-    match = abba_regex.match(address)
-    
-  return None
+    match = abba_regex.search(address)
+    if match:
+      if not inner_regex.search(address):
+        count += 1
+        
+  return count
 
-def execute_part_two(input):
-    return None
+def execute_part_two(ipv7s):
+  inside = re.compile(r'((\w)(?!\2)(\w)\2)(?![^\[]*\])(.*?\3\2\3)(?=[^\[]*\])') #checks aba[bab]
+  outside = re.compile(r'((\w)(?!\2)(\w)\2)(?=[^\[]*\])(.*?\3\2\3)(?![^\[]*\])') #checks [bab]aba
+  count = 0
+  for address in ipv7s:
+    if inside.search(address) or outside.search(address):
+      count += 1
+  return count
 
 def get_expected_results_map():
-    return { 'test.txt' : (None, None)}
+    return { 'test.txt' : (3, None), 'test2.txt' : (None, 3)}
