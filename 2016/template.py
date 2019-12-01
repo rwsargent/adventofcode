92d67cb71e1c32b3061135a63b0d15c0130d7e1c
rwsargent <rw.sarge@gmail.com>
2019-12-01 14:34:24 -0800

    Moved all files into 2016

--- 2016/template.py
+++ 2016/template.py
@@ -1,14 +1,18 @@
 def read_input(filename):
-  with open(filename) as file:
-    for line in file:
-      pass
-  return None
+    with open(filename) as file:
+        for line in file:
+            pass
+    return None
+        
+def execute_part_one(input, goal):
+    return None
 
-def execute_part_one(input):
-  return None
-
-def execute_part_two(input):
-  return None
+def execute_part_two(input, goal):
+    return None
 
 def get_expected_results_map():
-  return { 'test.txt' : (None, None)}
+    return { 'test.txt' : (None, None)}
+def get_test_goal():
+    return 0
+def get_problem_goal():
+    return 0
--- setup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-cp template.py $1.py
-mkdir tests/$1
-touch tests/$1/test.txt
-touch input/$1_input.txt
